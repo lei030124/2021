c54b896e72cea7511d8813759685e8911b3c20b4
lei030124 <71051430+lei030124@users.noreply.github.com>
2021-02-08 11:40:22 +0800

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+sd=eval(input())
+sb=eval(input())
+sa[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+sc[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+if sd in sa and sb in sa:
+    print(sa[sd:sb])
+else:
+    print(sc[sd:sb])
